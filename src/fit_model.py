8247f02ce96f3ade91c864b6db9036e87230011f
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-04-24 10:52:15 +0300

    add fitting workflow in test

--- src/fit_model.py
+++ src/fit_model.py
@@ -23,3 +23,52 @@ def Fit_Model(model_function, input_data, experimental_data, initial_params, par
         return -1
     else:
         return fit_parameters
+
+
+def Show_Params(params):
+    """
+    print the parameters to console
+    """
+    print(params[0], params[1], params[2])
+
+
+def Save_Params(params, path):
+    """
+    write parameters to file
+    """
+    with open(path, 'w+') as writer:
+        writer.write(str(params[0]))
+        writer.write("\n")
+        writer.write(str(params[1]))
+        writer.write("\n")
+        writer.write(str(params[2]))
+        writer.write("\n")
+
+
+def Fitting_Workflow():
+    """
+    Evaluate the TRM model via the fitting procedure defined in `Fit_Model`, but with different initial guesses for [I1,I2,I3]\n
+    GENERAL WORKFLOW ->\n
+    1. For every initial guess `p0_i` a set of fitting parameters will be obtained `P_fit_i(p0_i)`
+    2. For that particular `P_fit_i`, the chi_2 function (i.e., RMS) of the theoretical energies will be calculated
+    3. Choose the `P_fit_i` which gives the smallest chi_2 value
+    4. Return the best `P_fit_i`
+    """
+
+    test_array1 = [12]
+    test_array2 = [13]
+    test_array3 = [14]
+
+    for I1_guess in test_array1:
+        for I2_guess in test_array2:
+            for I3_guess in test_array3:
+                p0_i = [I1_guess, I2_guess, I3_guess]
+                print(p0_i)
+
+
+def main():
+    Fitting_Workflow()
+
+
+if __name__ == "__main__":
+    main()
